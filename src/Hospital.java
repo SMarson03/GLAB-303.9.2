162b33882d6f91b92201728f3e06b6889c16649b
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-15 14:38:20 -0400

    1

---
+++ src/Hospital.java
@@ -0,0 +1,9 @@
+public class Hospital {
+    public static void main(String[] args) {
+        Surgeon s = new Surgeon();
+        s.Doctor_Details();
+        s.Surgeon_Details();
+    }
+}
+//We learned this in class a few times I understand the parent
+//child relationship a little better
